4eccede2fbc01adb6f0ad8ee8b560a8b857e32b0
daevooh <97046712+daevooh@users.noreply.github.com>
2022-12-02 02:23:07 +0100

    empty line convention

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
   """Print all names defined by hidden_4 module."""
 import hidden_4
@@ -8,3 +7,4 @@ names = dir(hidden_4)
 for name in names:
     if name[:2] != "__":
         print(name)
+        
