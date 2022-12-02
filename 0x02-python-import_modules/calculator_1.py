8b2115762229f6aa97c2ca58ba744a7dd6ea4bc7
daevooh <97046712+daevooh@users.noreply.github.com>
2022-12-02 09:00:49 +0100

    file fix up

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,44 +1,20 @@
 #!/usr/bin/python3
 
 def add(a, b):
-    """My addition function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a + b
-    """
+    """My addition function"""
     return (a + b)
 
 
 def sub(a, b):
-    """My subtraction function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a - b
-    """
+    """My subtraction function"""
     return (a - b)
 
 
 def mul(a, b):
-    """My multiplication function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a * b
-    """
+    """My multiplication function"""
     return (a * b)
 
 
 def div(a, b):
-    """My division function
-    Args:
-        a: first integer
-        b: second integer
-    Returns:
-        The return value. a / b
-    """
+    """My division function"""
     return int(a / b)
